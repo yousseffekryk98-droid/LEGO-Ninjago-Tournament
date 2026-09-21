29310167bcda48b8ed0fa7d5d74f17307d843fbf
Youssef Mohamed Fekry Abdelghafar Nasr <yousseffekryk98@gmail.com>
2026-09-22 00:14:53 +0300

    refactor: make 3d model types shared and dependency-safe

---
+++ src/shared/three/model-types.ts
@@ -0,0 +1,31 @@
+export type FighterArchetype =
+  | 'ninja'
+  | 'nindroid'
+  | 'master'
+  | 'samurai'
+  | 'elemental'
+  | 'serpentine'
+  | 'skeleton'
+  | 'villain';
+
+export type FighterWeapon =
+  | 'katana'
+  | 'dual-katana'
+  | 'shuriken'
+  | 'scythe'
+  | 'nunchucks'
+  | 'staff'
+  | 'spear'
+  | 'claws'
+  | 'none';
+
+export interface FighterModelProfile {
+  archetype: FighterArchetype;
+  weapon: FighterWeapon;
+  hood: boolean;
+  shoulderArmor: boolean;
+  extraArms: boolean;
+  metallic: boolean;
+  faceColor?: number;
+  eyeColor?: number;
+}
